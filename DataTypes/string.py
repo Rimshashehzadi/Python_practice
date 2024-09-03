57bf4817286d58151212b666fdc522dc4cd91f20
rimsha_shehzadi <rimshashehzadi79@gmail.com>
2024-09-03 23:06:27 +0500

    strings

---
+++ DataTypes/string.py
@@ -0,0 +1,31 @@
+#string
+print("Hello")
+print('Hello')
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+a = "Hello"
+print(a)
+
+#Multi string
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+#string in array
+
+a = "Hello, World!"
+print(a[1])
+
+#string in loop
+for x in "banana":
+  print(x)
+
+  #length in string
+#   The len() function returns the length of a string:
+a = "Hello, World!"
+print(len(a))
