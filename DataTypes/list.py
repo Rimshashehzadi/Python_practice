419a17aff8d4e9e4945aa87b0142267b49eef7c8
rimsha_shehzadi <rimshashehzadi79@gmail.com>
2024-09-03 23:24:22 +0500

    List

--- DataTypes/list.py
+++ DataTypes/list.py
@@ -30,3 +30,60 @@ print(thislist)
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
 thislist[1:3] = ["blackcurrant", "watermelon"]
 print(thislist)
+
+#Add items in list
+# Using the append() method to append an item:
+
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+# Insert an item as the second position:
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+
+#To append elements from another list to the current list, use the extend() method
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+#Remove items
+
+#The remove() method removes the specified item.
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+#The pop() method removes the specified index.
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+
+#remove the last item
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+
+#The clear() method empties the list.
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
+
+#loop
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+  #for loop
+  thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+
+  #comprehensive
+  thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
